55c9fb4979c628078ad9698633468b526239c963
izabelka <i.konstanciak@gmail.com>
2017-01-30 20:47:19 +0100

    Add buttons component

---
+++ src/Buttons.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+const buttons = Array.apply(null, {length: 10}).map(Number.call, Number)
+
+
+class Buttons extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+
+ onButtonClick(item, e) {  
+  console.log(item);
+}
+
+	render() {
+		return (
+    	<div className="calculator-buttons">
+      	{buttons.map((button) =>
+        	<span key={button.toString()}
+                  value={button} onClick={this.onButtonClick.bind(this, button)}> {button} </span>
+      	)}
+    	</div>
+  	);
+  }
+}
+
+export default Buttons;
